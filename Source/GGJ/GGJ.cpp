9a00485a6010050101528dde50657f15f0be019f
Michał Ogiński <70843043+intrxx@users.noreply.github.com>
2023-02-03 15:42:28 +0100

    Init.

---
+++ Source/GGJ/GGJ.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GGJ.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJ, "GGJ" );
